cbfc189f9899ba58dbca4fbaaaa858c45d05313e
Isa Mary <isa.kusuki@gmail.com>
2023-05-28 17:22:57 -0300

    Criação do projeto

---
+++ deGraoEmGrao/src/main/java/br/com/deGraoEmGrao/DeGraoEmGraoApplication.java
@@ -0,0 +1,13 @@
+package br.com.deGraoEmGrao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeGraoEmGraoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeGraoEmGraoApplication.class, args);
+	}
+
+}
